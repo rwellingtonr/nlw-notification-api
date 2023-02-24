913b71781dc1fdcbb3bd9e11ffe782ffa7e0f03b
Wellington <wellington-158@hotmail.com>
2023-02-23 21:55:39 -0300

    feat: habilitado cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
-}
+  app.enableCors();
+  await app.listen(process.env.PORT);
+};
 bootstrap();
